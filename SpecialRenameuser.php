952cd228226c0f76ee05d48c30a8b80bb549d086
Aaron Schulz <aaron@users.mediawiki.org>
2007-06-04 19:25:56 +0000

    *Revert, causes "class SpecialPage not found error"

--- SpecialRenameuser.php
+++ SpecialRenameuser.php
@@ -24,7 +24,6 @@ $wgExtensionCredits['specialpage'][] = array(
 
 # Internationalisation file
 require_once( 'SpecialRenameuser.i18n.php' );
-require_once( 'SpecialRenameuser_body.php' );
 
 /**
  * The maximum number of edits a user can have and still be allowed renaming,
@@ -51,11 +50,10 @@ $wgRenameUserQuick = $wgMiserMode;
 if ( !function_exists( 'extAddSpecialPage' ) ) {
 	require( dirname(__FILE__) . '/../ExtensionFunctions.php' );
 }
+extAddSpecialPage( dirname(__FILE__) . '/SpecialRenameuser_body.php', 'Renameuser', 'Renameuser' );
 
 function wfSpecialRenameuser() {
 	# Add messages
-	//extAddSpecialPage( dirname(__FILE__) . '/SpecialRenameuser_body.php', 'Renameuser', 'Renameuser' );
-	SpecialPage::AddPage(new Renameuser());	
 	global $wgMessageCache, $wgRenameuserMessages;
 	foreach( $wgRenameuserMessages as $key => $value ) {
 		$wgMessageCache->addMessages( $wgRenameuserMessages[$key], $key );
