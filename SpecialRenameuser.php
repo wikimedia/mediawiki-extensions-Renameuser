f0b50a64c341d4439e7b3f192b8ec47ba933b5d2
Aaron Schulz <aaron@users.mediawiki.org>
2007-09-25 11:37:20 +0000

    *Revert 25997, breaks sp:log message. (bug 11446)

--- SpecialRenameuser.php
+++ SpecialRenameuser.php
@@ -22,6 +22,8 @@ $wgExtensionCredits['specialpage'][] = array(
 	'description' => 'Rename a user (need \'\'renameuser\'\' right)',
 );
 
+# Internationalisation file
+require_once( 'SpecialRenameuser.i18n.php' );
 
 /**
  * The maximum number of edits a user can have and still be allowed renaming,
@@ -42,20 +44,9 @@ $wgAutoloadClasses['RenameUserJob'] = dirname(__FILE__) . '/RenameUserJob.php';
 $wgSpecialPages['Renameuser'] = 'SpecialRenameuser';
 $wgJobClasses['renameUser'] = 'RenameUserJob';
 
-$wgHooks['LoadAllMessages'][] = 'wfSpecialRenameuserMessages';
-
 function wfSpecialRenameuser() {
-	global $wgVersion;
-	if ( version_compare( $wgVersion, "1.8", '<' ) ) {
-		wfSpecialRenameuserMessages();
-	}
-}
-
-function wfSpecialRenameuserMessages() {
-	require( 'SpecialRenameuser.i18n.php' );
-	global $wgMessageCache;
+	global $wgMessageCache, $wgRenameuserMessages;
 	foreach( $wgRenameuserMessages as $lang => $messages ) {
 		$wgMessageCache->addMessages( $messages, $lang );
 	}
-	return true;
 }
