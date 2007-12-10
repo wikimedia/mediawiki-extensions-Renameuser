39415b513321ebf79cdbdcf2495ea6d570afb816
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-10 21:14:34 +0000

    * complete $wgExtensionCredits * remove trailing whitespace

--- SpecialRenameuser.php
+++ SpecialRenameuser.php
@@ -17,6 +17,7 @@ $wgGroupPermissions['bureaucrat']['renameuser'] = true;
 $wgExtensionFunctions[] = 'wfSpecialRenameuser';
 $wgExtensionCredits['specialpage'][] = array(
 	'name' => 'Renameuser',
+	'version' => '1.1',
 	'author' => 'Ævar Arnfjörð Bjarmason',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:Renameuser',
 	'description' => 'Rename a user (need \'\'renameuser\'\' right)',
