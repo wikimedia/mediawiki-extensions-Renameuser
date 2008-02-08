c729d78159353d42638571f44adafc37933ea6c6
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-08 13:28:20 +0000

    Remove forced fall back on extension level. No longer needed. This was resolved in r30440 for extensions using wfLoadExtensionMessages.

--- SpecialRenameuser.i18n.php
+++ SpecialRenameuser.i18n.php
@@ -1657,16 +1657,3 @@ $messages['zh-hant'] = array(
 $messages['zu'] = array(
 	'renameusersubmit' => 'Yisa',
 );
-
-$messages['de-formal'] = $messages['de'];
-$messages['kk'] = $messages['kk-cyrl'];
-$messages['kk-cn'] = $messages['kk-arab'];
-$messages['kk-kz'] = $messages['kk-cyrl'];
-$messages['kk-tr'] = $messages['kk-latn'];
-$messages['zh'] = $messages['zh-hans'];
-$messages['zh-cn'] = $messages['zh-hans'];
-$messages['zh-hk'] = $messages['zh-hant'];
-$messages['zh-min-nan'] = $messages['nan'];
-$messages['zh-sg'] = $messages['zh-hans'];
-$messages['zh-tw'] = $messages['zh-hant'];
-$messages['zh-yue'] = $messages['yue'];
