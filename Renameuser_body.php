83b07b35f88e0fb1b46bb278a5526d6c37b7a98a
Siebrand Mazeland <s.mazeland@xs4all.nl>
2012-08-29 16:56:07 +0200

    Update logging to latest standards, including pretty formatting.
    Compatible with MediaWiki 1.19 and later.
    
    Change-Id: I4be1fdb79d1c1008b033feb58ee20c6b059c0c46

--- Renameuser_body.php
+++ Renameuser_body.php
@@ -279,14 +279,19 @@ class SpecialRenameuser extends SpecialPage {
 			$user->setName( $newusername->getText() );
 		}
 
-		// Log this rename
-		$log = new LogPage( 'renameuser' );
-		$log->addEntry(
-			'renameuser',
-			$oldusername,
-			$this->msg( 'renameuser-log' )->numParams( $contribs )->params( $reason )->inContentLanguage()->text(),
-			$newusername->getText()
-		);
+		// Log this rename, updated to 1.19+ Log form.
+		//	https://www.mediawiki.org/wiki/Logging_to_Special:Log
+		$logEntry = new ManualLogEntry( 'renameuser', 'renameuser' );
+		$logEntry->setPerformer( $wgUser );
+		$logEntry->setTarget( $oldusername );
+		$logEntry->setComment( $reason );
+		$logEntry->setParameters( array(
+			'4::olduser' => $oldusername->getText(),
+			'5::newuser' => $newusername->getText(),
+			'6::edits' => $contribs
+		) );
+		$logid = $logEntry->insert();
+		$logEntry->publish( $logid );
 
 		// Move any user pages
 		if ( $request->getCheck( 'movepages' ) && $user->isAllowed( 'move' ) ) {
@@ -593,5 +598,71 @@ class RenameuserSQL {
 
 		wfProfileOut( __METHOD__ );
 		return true;
+	}
+}
+
+class RenameuserLogFormatter extends LogFormatter {
+	protected function getMessageParameters() {
+		$params = parent::getMessageParameters();
+		/* Current format:
+		 * 1,2,3: normal logformatter params
+		 * 4: old username
+		 *    (legaciest doesn't have this at all, all in comment)
+		 *    (legacier uses this as new name and stores old name in target)
+		 * 5: new username
+		 * 6: number of edits the user had at the time
+		 *    (not available except in newest log entries)
+		 * Note that the arrays are zero-indexed, while message parameters
+		 * start from 1, so substract one to get array entries below.
+		 */
+
+		if ( !isset( $params[3] ) ) {
+			// The oldest format
+			return $params;
+		} elseif ( !isset( $params[4] ) ) {
+			// See comments above
+			$params[4] = $params[3];
+			$params[3] = $this->entry->getTarget()->getText();
+		}
+
+		// Nick link to old user page
+		$title = Title::makeTitleSafe( NS_USER, $params[3] );
+		$link = $this->myPageLink( $title, $params[3] );
+		$params[3] = Message::rawParam( $link );
+
+		// Nice link to new user page
+		$title = Title::makeTitleSafe( NS_USER, $params[4] );
+		$link = $this->myPageLink( $title, $params[4] );
+		$params[4] = Message::rawParam( $link );
+
+		return $params;
+	}
+
+	protected function myPageLink( Title $title = null, $text ) {
+		if ( !$this->plaintext ) {
+			$text = htmlspecialchars( $text );
+			$link = Linker::link( $title, $text );
+		} else {
+			if ( !$title instanceof Title ) {
+				$link = "[[User:$text]]";
+			} else {
+				$link = '[[' . $title->getPrefixedTextText() . ']]';
+			}
+		}
+		return $link;
+	}
+
+	public function getMessageKey() {
+		$key = parent::getMessageKey();
+		$params = $this->extractParameters();
+
+		// Very old log format, everything in comment
+		if ( !isset( $params[3] ) ) {
+			return "$key-legaciest";
+		} elseif ( !isset( $params[5] ) ) {
+			return "$key-legacier";
+		}
+
+		return $key;
 	}
 }
