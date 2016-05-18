08e6e9bcaf2fcf483b3a9953bf8f418823ffdce1
Kunal Mehta <legoktm@member.fsf.org>
2016-05-18 10:28:46 -0700

    Load User with READ_LATEST before invalidating sessions
    Bug: T135656
    Change-Id: I4dc602a5bea7360575d2fbebf7309df800035515

--- RenameuserSQL.php
+++ RenameuserSQL.php
@@ -196,6 +196,7 @@ class RenameuserSQL {
 		if ( class_exists( SessionManager::class ) &&
 			is_callable( [ SessionManager::singleton(), 'invalidateSessionsForUser' ] )
 		) {
+			$user->load( User::READ_LATEST );
 			SessionManager::singleton()->invalidateSessionsForUser( $user );
 		} else {
 			$authUser = $wgAuth->getUserInstance( $user );
