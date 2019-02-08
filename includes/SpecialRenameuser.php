ac5318b82e2dd275eec5fa545db542326e1878bf
Umherirrender <umherirrender_de.wp@web.de>
2019-02-08 21:13:50 +0100

    Rename variable to avoid clutter in taint-check-plugin
    When a variable is reused in the code taint-check-plugin has trouble to
    keep the tainted state for the variable
    
    Bug: T211471
    Change-Id: I289e219b0a87bcc1ddcdbc0e98f623b09042c78f

--- includes/SpecialRenameuser.php
+++ includes/SpecialRenameuser.php
@@ -352,14 +352,14 @@ class SpecialRenameuser extends SpecialPage {
 						$this->msg( 'renameuser-page-exists' )->rawParams( $link )->escaped()
 					);
 				} else {
-					$reason = $this->msg(
+					$logReason = $this->msg(
 						'renameuser-move-log', $oldusername->getText(), $newusername->getText()
 					)->inContentLanguage()->text();
 
 					$success = $oldPage->moveTo(
 						$newPage,
 						false,
-						$reason,
+						$logReason,
 						!$suppressRedirect
 					);
 					if ( $success === true ) {
