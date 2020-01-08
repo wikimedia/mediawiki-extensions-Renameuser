11065d165311a009e963bbd8555c4a9fab95a101
Umherirrender <umherirrender_de.wp@web.de>
2020-01-08 19:25:44 +0000

    Revert "build: Updating npm dependencies"
    This reverts commit a8dd06967e74cac17885a7372334df5a9c20b0c5.
    
    Change-Id: I4009808ea6a701cd854712385476379aba8fb58b

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,7 @@ module.exports = function ( grunt ) {
 	grunt.initConfig( {
 		eslint: {
 			options: {
+				reportUnusedDisableDirectives: true,
 				extensions: [ '.js', '.json' ],
 				cache: true
 			},
