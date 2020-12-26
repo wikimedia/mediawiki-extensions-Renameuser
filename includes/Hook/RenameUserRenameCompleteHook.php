e2df2dbcebc6966eabaca1c0221768df22a15c9e
Taavi Väänänen <hi@tassu.me>
2020-12-26 16:44:01 +0200

    Update to use the new HookContainer/HookRunner system
    Bump MediaWiki requirement to 1.35+ to use the new hook system
    
    Bug: T270835
    Change-Id: I082f5edaecb1713cf1dbd39df22d6f1c5a9f1a06

---
+++ includes/Hook/RenameUserRenameCompleteHook.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This is a hook handler interface, see docs/Hooks.md.
+ * Use the hook name "RenameUserRenameComplete" to register handlers implementing this interface.
+ *
+ * @stable to implement
+ * @ingroup Hooks
+ * @since 1.36
+ */
+interface RenameUserRenameCompleteHook {
+
+	/**
+	 * @param int $uid The user ID
+	 * @param string $old The old username
+	 * @param string $new The new username
+	 */
+	public function onRenameUserRenameComplete( int $uid, string $old, string $new ) : void;
+
+}
